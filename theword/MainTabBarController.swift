da9e0954753c2b05e72ea16cfd0675909dddfcf7
Wilbert Liu <wilbertliu@gmail.com>
2016-12-11 23:35:46 +0700

    Initial commit

---
+++ theword/MainTabBarController.swift
@@ -0,0 +1,17 @@
+//
+//  MainTabBarController.swift
+//  theword
+//
+//  Created by Wilbert Liu on 12/11/16.
+//  Copyright © 2016 Ident. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
---
+++ theword/MainViewController.swift
@@ -0,0 +1,21 @@
+//
+//  MainViewController.swift
+//  theword
+//
+//  Created by Wilbert Liu on 12/10/16.
+//  Copyright © 2016 Ident. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+
+}
